86f3001faebe1743352b77c8b05c247b3976578e
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-01-10 22:18:52 -0500

    rolled back to browserrouter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import Main from './components/MainComponent';
 import ScrollToTop from './components/ScrollToTopComponent';
 import './App.scss';
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <ScrollToTop/>
       <div className="App">
         <Main/>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
