48827af024e9a7587926c3e3383e6398ef1a334d
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-08-27 13:33:50 -0400

    adds Provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import Main from './components/MainComponent';
-import ScrollToTop from './components/ScrollToTopComponent';
-import './App.scss';
-import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom';
+import Main from "./components/MainComponent";
+import ScrollToTop from "./components/ScrollToTopComponent";
+import "./App.scss";
+import React, { Component } from "react";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+
+const store = ConfigureStore();
 
 function App() {
   return (
-    <HashRouter>
-      <ScrollToTop/>
-      <div className="App">
-        <Main/>
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <ScrollToTop />
+        <div className="App">
+          <Main />
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
